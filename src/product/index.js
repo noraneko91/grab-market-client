aa1800e998b33b5ef97d4c764ea90d61365750b8
sungtae <ttvt11@gmail.com>
2023-05-21 23:11:36 +0900

    슬라이딩배너,결제하기 구현 반응형 웹구현

--- src/product/index.js
+++ src/product/index.js
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react";
 import "./index.css";
 import { API_URL } from "../config/constants";
 import dayjs from "dayjs";
+import { Button, message } from "antd";
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  useEffect(function () {
+  const getProduct = () => {
     axios
       .get(
         //`https://a6b99d19-fcd3-4766-997b-556cccb12f63.mock.pstmn.io/products/${id}`
@@ -20,11 +21,25 @@ function ProductPage() {
       .catch(function (error) {
         console.error(error);
       });
+  };
+
+  useEffect(function () {
+    getProduct();
   }, []);
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다..</h1>;
   }
-
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
   return (
     <div>
       <div id="image-box">
@@ -40,6 +55,16 @@ function ProductPage() {
         <div id="createdAt">
           {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
         </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1}
+        >
+          재빨리 구매하기
+        </Button>
         <pre id="description">{product.description} </pre>
       </div>
     </div>
